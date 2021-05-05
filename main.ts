2494f43ee2786d7743ede80729d759e73d102cf2
aliabood-coder <83079653+aliabood-coder@users.noreply.github.com>
2021-05-05 10:36:00 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,32 @@
+let item = 0
+input.onGesture(Gesture.Shake, function () {
+    item = randint(0, 2)
+    if (item == 0) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else if (item == 1) {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    } else {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    }
+})
+basic.forever(function () {
+	
+})
